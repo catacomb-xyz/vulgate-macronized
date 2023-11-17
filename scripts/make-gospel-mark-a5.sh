81bffe070bdfb9b4b0afde405e15f8dc5508f736
anon <anon@debian-BULLSEYE-live-builder-AMD64>
2023-11-16 20:44:37 -0800

    check in items needed to build gospel of mark

--- scripts/make-gospel-mark-a5.sh
+++ scripts/make-gospel-mark-a5.sh
@@ -144,23 +144,107 @@ echo """
 \usepackage{setspace}
 
 \begin{document}
+
+% TITLE PAGE %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 \thispagestyle{empty} 
 \begin{center}
 \topskip0pt
 \vspace*{\fill}
 {\huge\bfseries\color{red} ĒVANGELIUM}
-
-\vspace{3mm}
+\vspace{2ex}
 
 {\large\bfseries\color{red} SECUNDUM MARCUM}
-
-\vspace{5mm}
+\vspace{8ex}
 
 {\huge\color{red} ✠}
 \vspace*{\fill}
 \end{center}
+\newpage
+
+% INFO PAGE %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+\thispagestyle{empty} 
+\begin{flushleft}
+\topskip0pt
+\vspace*{\fill}
+\begin{footnotesize}
+\noindent
+Evangelium Secundum Marcum, build date: \today
+
+\noindent
+This is a free and unencumbered work released into the public domain.
+
+\noindent
+Anyone is free to copy, modify, publish, use, sell, or distribute this work, either in print or digital form, for any purpose, commercial or non-commercial, and by any means.
+
+\noindent
+In jurisdictions that recognize copyright laws, the author or authors of this work dedicate any and all copyright interest in the work to the public domain. We make this dedication for the benefit of the public at large and to the detriment of our heirs and successors. We intend this dedication to be an overt act of relinquishment in perpetuity of all present and future rights to this document under copyright law.
+
+\noindent
+THE WORK IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE WORK OR THE USE OR OTHER DEALINGS IN THE WORK.
+\vspace{4ex}
+
+\noindent
+For further information, comments, questions, or corrections, please visit \textbf{doctrinalatina.com}.
+
+\end{footnotesize}
+\vspace*{\fill}
+\end{flushleft}
+\newpage
+
+% INTRO PRAYER PAGE %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+\thispagestyle{empty} 
+\begin{center}
+\topskip0pt
+\vspace*{\fill}
+
+\noindent{\large\bfseries\color{red} Ōrātiō ad Sānctum Spīritum}\vspace{-3ex}
+
+\noindent\rule{3in}{0.1pt}
+
+\noindent{Venī, Sānctē Spīritus, replē tuōrum corda fidēlium,}\vspace{-2ex}
+
+\noindent\emph{\scriptsize Come, O Holy Spirit, fill the hearts of thy faithful,}
+
+\noindent{et tuī amōris in eīs ignem accende.}\vspace{-2ex}
+
+\noindent\emph{\scriptsize and kindle in them the fire of thy love.}
+
+\noindent{Ēmitte Spīritum tuum et creābuntur,}\vspace{-2ex}
+
+\noindent\emph{\scriptsize Send forth thy Spirit and they shall be created,}
+
+\noindent{et renovābis faciem terrae.}\vspace{-2ex}
+
+\noindent\emph{\scriptsize and thou shalt renew the face of the earth.}
+
+\noindent{Deus, quī corda fidēlium Sānctī Spīritus illūstrātiōne docuistī,}\vspace{-2ex}
+
+\noindent\emph{\scriptsize O God, who didst instruct the hearts of the faithful by the light of the Holy Spirit,}
+
+\noindent{dā nōbīs in eōdem Spīritū rēcta sapere,}\vspace{-2ex}
+
+\noindent\emph{\scriptsize grant us in the same Spirit to be truly wise,}
+
+\noindent{et dē eius semper cōnsōlātiōne gaudēre.}\vspace{-2ex}
+
+\noindent\emph{\scriptsize and ever rejoice in his consolation.}
+
+\noindent{Per Chrīstum Dominum nostrum. Āmēn.}\vspace{-2ex}
+
+\noindent\emph{\scriptsize Through Christ our Lord. Amen.}
+
+\vspace{4ex}
+
+\includegraphics[scale=0.55]{red-dove.png}
+
+\vspace*{\fill}
+\end{center}
 
 \newpage
+
+% BEGIN GOSPEL OF MARK %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 \clearpage
 \pagenumbering{arabic}
 \begin{flushleft}
